5dd56a8ab08539cb8506f9a1df4a618fbb9aae40
granj2020 <61171319+granj2020@users.noreply.github.com>
2021-02-27 14:07:32 -0400

    Update Collision.h

--- include/Collision.h
+++ include/Collision.h
@@ -1,13 +1,12 @@
 /*************************************************************************
-* Copyright (c) 2019-2021 Jonathan Peña
+* Copyright (c) 2019-2021 Jonathan PeÃ±a
 * Permission to use, copy, modify, distribute and sell this software
 * and its documentation for any purpose is hereby granted without fee,
 * provided that the above copyright notice appear in all copies.
-* Jonathan Peña makes no representations about the suitability 
+* Jonathan PeÃ±a makes no representations about the suitability 
 * of this software for any purpose.  
 * It is provided "as is" without express or implied warranty. 
 **************************************************************************/
-
 
 #ifndef COLLISION_H
 #define COLLISION_H
